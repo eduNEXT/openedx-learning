4e759834f34edee9070136ef78af26a32b11643d
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-11-27 15:15:30 +0100

    Proposal: use Unit > Selector > Segment > SegmentRow > PublishableEntity composition

--- openedx_learning/apps/authoring/containers/models.py
+++ openedx_learning/apps/authoring/containers/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from openedx_learning.apps.authoring.containers.models_mixin import ContainerEntityVersionMixin
 from openedx_learning.apps.authoring.publishing.models import (
     PublishableEntity,
     PublishableEntityVersion,
@@ -11,33 +10,33 @@ from ..publishing.model_mixins import (
 )
 
 
-class EntityList(models.Model):
+class Segment(models.Model):
     """
-    EntityLists are a common structure to hold parent-child relations.
+    Segments are a common structure to hold parent-child relations.
 
-    EntityLists are not PublishableEntities in and of themselves. That's because
+    A selector can have multiple segments, each of which can have multiple
+    SegmentRows. The SegmentRows are the actual members of the Segment.
+
+    Segments are not PublishableEntities in and of themselves. That's because
     sometimes we'll want the same kind of data structure for things that we
-    dynamically generate for individual students (e.g. Variants). EntityLists are
+    dynamically generate for individual students (e.g. Variants). Segments are
     anonymous in a sense–they're pointed to by ContainerEntityVersions and
     other models, rather than being looked up by their own identifers.
     """
 
-    pass
-
-
-class EntityListRow(models.Model):
+class SegmentRow(models.Model):
     """
-    Each EntityListRow points to a PublishableEntity, optionally at a specific
+    Each SegmentRow points to a PublishableEntity, optionally at a specific
     version.
 
-    There is a row in this table for each member of an EntityList. The order_num
+    There is a row in this table for each member of an Segment. The order_num
     field is used to determine the order of the members in the list.
     """
 
-    entity_list = models.ForeignKey(EntityList, on_delete=models.CASCADE)
+    segment = models.ForeignKey(Segment, on_delete=models.CASCADE)
 
     # This ordering should be treated as immutable–if the ordering needs to
-    # change, we create a new EntityList and copy things over.
+    # change, we create a new Segment and copy things over.
     order_num = models.PositiveIntegerField()
 
     # Simple case would use these fields with our convention that null versions
@@ -48,8 +47,8 @@ class EntityListRow(models.Model):
     entity = models.ForeignKey(PublishableEntity, on_delete=models.RESTRICT)
 
     # The version references point to the specific PublishableEntityVersion that
-    # this EntityList has for this PublishableEntity for both the draft and
-    # published states. However, we don't want to have to create new EntityList
+    # this Segment has for this PublishableEntity for both the draft and
+    # published states. However, we don't want to have to create new Segment
     # every time that a member is updated, because that would waste a lot of
     # space and make it difficult to figure out when the metadata of something
     # like a Unit *actually* changed, vs. when its child members were being
@@ -104,16 +103,11 @@ class ContainerEntityVersion(PublishableEntityVersionMixin):
         related_name="versions",
     )
 
-    # This is the EntityList that the author defines. This should never change,
-    # even if the things it references get soft-deleted (because we'll need to
-    # maintain it for reverts).
-    defined_list = models.ForeignKey(
-        EntityList,
-        on_delete=models.RESTRICT,
-        null=False,
-        related_name="defined_list",
-    )
+    # # Selector to get the content from. Selector > SelectorVersion > Segment
+    # selector = models.ForeignKey(Selector, on_delete=models.RESTRICT)
 
+    # # Version of the Selector to get the content from. SelectorVersion > Segment
+    # selector_version = models.ForeignKey(SelectorVersion, on_delete=models.RESTRICT)
 
 class Selector(PublishableEntityMixin):
     """
@@ -129,7 +123,9 @@ class Selector(PublishableEntityMixin):
 
     A Selector has versions.
     """
-    pass
+    container = models.ForeignKey(ContainerEntity, on_delete=models.RESTRICT)
+
+    container_version = models.ForeignKey(ContainerEntityVersion, on_delete=models.RESTRICT)
 
 
 class SelectorVersion(PublishableEntityVersionMixin):
@@ -141,6 +137,24 @@ class SelectorVersion(PublishableEntityVersionMixin):
     must have is a foreign key to Selector, and Variants that point to it.
     """
     selector = models.ForeignKey(Selector, on_delete=models.RESTRICT)
+
+    # This is used to determine the order of the selectors in the container.
+    # Simple case would be having a single selector in a container so order
+    # doesn't matter (static members), but we might want to have multiple selectors in a
+    # container, e.g. for non-static content like A/B tests.
+    # Selector 0 (type: static)
+    # - Segment (single segment, this is just a list of members. If I add more members here, I'd need to know whether to add it before or after selector 1)
+    #    - Text
+    #    - Multiple choice
+    # Selector 1 (type: A/B Test)
+    # - Segment
+    #   - Variant (Problem selected for this user)
+    order_num = models.PositiveIntegerField()
+
+    # The segment that this selector version gets its content from.
+    # Segment > SegmentRow > PublishableEntityVersion or
+    # Segment > Variant?
+    segment = models.ForeignKey(Segment, on_delete=models.RESTRICT)
 
 
 class Variant(models.Model):
@@ -155,5 +169,4 @@ class Variant(models.Model):
     Selector, and that mapping will happen via a model in the ``learning``
     package).
     """
-    entity_list = models.OneToOneField(EntityList, on_delete=models.RESTRICT, primary_key=True)
-    selector_version = models.ForeignKey(SelectorVersion, on_delete=models.RESTRICT)
+    segment = models.OneToOneField(Segment, on_delete=models.RESTRICT, primary_key=True)
